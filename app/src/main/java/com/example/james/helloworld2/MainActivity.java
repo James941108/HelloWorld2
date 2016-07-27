67e3feec1662c4cf5807a242baa6c50fd8d00489
chaowakritwimon <fluk_kulf2538@hotmail.com>
2016-07-27 15:25:44 +0700

    Add 's' in Result in Toast MainActivity

--- app/src/main/java/com/example/james/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/james/helloworld2/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tvResult.setText(result + "");
 
             Log.d("Calculations", "Result = " + result);
-            Toast.makeText(MainActivity.this, "Result = " + result, Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Results = " + result, Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(MainActivity.this,
                     SecondActivity.class);
